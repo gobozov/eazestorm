2f85515848982e14e9e8bf898f46c6f509f0193b
gobozov <gobozov@gmail.com>
2012-12-03 17:26:59 +0400

    web dir settings

--- src/ru/eaze/settings/SettingsForm.java
+++ src/ru/eaze/settings/SettingsForm.java
@@ -1,9 +1,11 @@
 package ru.eaze.settings;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileSystemTree;
 import com.intellij.openapi.fileChooser.FileSystemTreeFactory;
 import com.intellij.openapi.fileChooser.ex.FileSystemTreeFactoryImpl;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.IdeBorderFactory;
 import com.intellij.uiDesigner.core.Spacer;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +14,10 @@ import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 import java.awt.*;
 
 /**
@@ -22,60 +28,93 @@ import java.awt.*;
  * To change this template use File | Settings | File Templates.
  */
 public class SettingsForm extends JPanel {
+
+    private Logger LOGGER = Logger.getInstance(SettingsForm.class);
     @NotNull
     private JTextField webDirTextField;
     @NotNull
     private Settings settings;
     private boolean isModified;
+    private String value;
 
     public SettingsForm(Settings settings) {
         this.settings = settings;
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        @NotNull final JPanel cachePanel = new JPanel(new BorderLayout());
+        @NotNull final JPanel treePanel = new JPanel(new BorderLayout());
         @NotNull final Border border = new CompoundBorder(new EtchedBorder(EtchedBorder.LOWERED), new EmptyBorder(3, 5, 3, 5));
-        cachePanel.setBorder(new TitledBorder(border, "EazeStorm Settings"));
+        treePanel.setBorder(new TitledBorder(border, "EazeStorm Settings"));
 
         @NotNull final JPanel gridWrapper = new JPanel(new GridLayout(1, 2));
         gridWrapper.add(new JLabel("Web directory name"));
         gridWrapper.add(new JLabel());
-        cachePanel.add(gridWrapper, BorderLayout.NORTH);
+        treePanel.add(gridWrapper, BorderLayout.NORTH);
 
-        webDirTextField = new JTextField(settings.getStringValue(Settings.KEY_WEB_DIR, "web"));
+//        webDirTextField = new JTextField(settings.getStringValue(Settings.KEY_WEB_DIR, "web"));
+//
+//        webDirTextField.getDocument().addDocumentListener(new DocumentListener() {
+//            public void changedUpdate(DocumentEvent e) {
+//                isModified = true;
+//            }
+//
+//            public void removeUpdate(DocumentEvent e) {
+//                isModified = true;
+//            }
+//
+//            public void insertUpdate(DocumentEvent e) {
+//                isModified = true;
+//            }
+//        });
+//
+//
+//        @NotNull JPanel buttonWrapper = new JPanel(new GridLayout(1, 3));
+//        buttonWrapper.setBorder(IdeBorderFactory.createEmptyBorder(3));
+//        buttonWrapper.add(webDirTextField);
+//        buttonWrapper.add(new Spacer());
+//        buttonWrapper.add(new Spacer());
+//        cachePanel.add(buttonWrapper, BorderLayout.SOUTH);
+//
+//        cachePanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, cachePanel.getPreferredSize().height));
+//        add(cachePanel);
 
-        webDirTextField.getDocument().addDocumentListener(new DocumentListener() {
-            public void changedUpdate(DocumentEvent e) {
-                isModified = true;
-            }
+        FileSystemTreeFactory treeFactory = new FileSystemTreeFactoryImpl();
+        FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
+        descriptor.setIsTreeRootVisible(true);
+        descriptor.setRoots(settings.getProject().getBaseDir());
+        final FileSystemTree tree = treeFactory.createFileSystemTree(settings.getProject(), descriptor);
+        final JTree jTree = tree.getTree();
 
-            public void removeUpdate(DocumentEvent e) {
-                isModified = true;
-            }
+        jTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
 
-            public void insertUpdate(DocumentEvent e) {
-                isModified = true;
+                if (e.getPath() != null){
+                    String webDirPath = e.getPath().getLastPathComponent().toString();
+                    LOGGER.info("WebDir lasp path = " + webDirPath);
+                    LOGGER.info("tree path = " + tree.getSelectedFile().getName());
+                    value = webDirPath;
+                    isModified = true;
+                }
             }
         });
 
+        String webDirPath = settings.getStringValue(Settings.KEY_WEB_DIR, "web");
+        //TreePath treePath = new TreePath(webDirPath);
+        //jTree.expandPath(treePath);
+        //jTree.getSelectionModel().setSelectionPath(null);
 
-        @NotNull JPanel buttonWrapper = new JPanel(new GridLayout(1, 3));
-        buttonWrapper.setBorder(IdeBorderFactory.createEmptyBorder(3));
-        buttonWrapper.add(webDirTextField);
-        buttonWrapper.add(new Spacer());
-        buttonWrapper.add(new Spacer());
-        cachePanel.add(buttonWrapper, BorderLayout.SOUTH);
+       // VirtualFile webDir = settings.getProject().getBaseDir().findFileByRelativePath(webDirPath);
+       // tree.select(webDir, null);
 
-        cachePanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, cachePanel.getPreferredSize().height));
-        add(cachePanel);
-
-        FileSystemTreeFactory treeFactory = new FileSystemTreeFactoryImpl();
-        FileChooserDescriptor descriptor =  new FileChooserDescriptor(false, true, false, false,false,false);
-        descriptor.setIsTreeRootVisible(true);
-        descriptor.setRoots(settings.getProject().getBaseDir());
-        FileSystemTree tree = treeFactory.createFileSystemTree(settings.getProject(), descriptor);
-        add(tree.getTree());
+//        if (settings.getStringValue(Settings.KEY_WEB_DIR, "web") != null){
+//            TreePath webDirPath = new TreePath(value);
+//            jTree.expandPath(webDirPath);
+//            jTree.setSelectionPath(webDirPath);
+//        }
 
 
+        treePanel.add(jTree, BorderLayout.CENTER);
+        add(treePanel);
 
 
     }
