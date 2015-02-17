29358a7218db7beb066dea929708ad82157c7a9a
Olesia Lavrichenko <lisovenok@gmail.com>
2015-02-18 00:48:38 +0300

    localization keys completion fix

--- src/ru/eaze/locale/completion/EazeLocaleCompletionProvider.java
+++ src/ru/eaze/locale/completion/EazeLocaleCompletionProvider.java
@@ -5,27 +5,23 @@ import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.ProcessingContext;
 import com.intellij.util.Processor;
 import com.intellij.util.indexing.FileBasedIndex;
+import org.jetbrains.annotations.NotNull;
 import ru.eaze.domain.EazeProjectStructure;
 import ru.eaze.locale.EazeLocaleDeclaration;
 import ru.eaze.locale.EazeLocaleDeclarationSearcher;
 import ru.eaze.locale.EazeLocaleKeyIndex;
 import ru.eaze.locale.EazeLocaleUtil;
-import ru.eaze.locale.findUsages.EazeLocaleUsagesIndex;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class EazeLocaleCompletionProvider extends CompletionProvider<CompletionParameters> {
     @Override
-    protected void addCompletions(CompletionParameters parameters, ProcessingContext context, CompletionResultSet result) {
+    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
         if (!EazeLocaleUtil.inScope(parameters.getOriginalPosition())) {
             return;
         }
@@ -39,17 +35,17 @@ public class EazeLocaleCompletionProvider extends CompletionProvider<CompletionP
             Processor<String> processor = new Processor<String>() {
                 @Override
                 public boolean process(String key) {
-                    Collection<VirtualFile> files = FileBasedIndex.getInstance().getContainingFiles(EazeLocaleUsagesIndex.NAME, key, scope);
-                    if (files.size() > 0) {
-                        if (completionType == CompletionType.SMART) {
-                            if (key.startsWith(prefix)) {
-                                int end = key.indexOf(EazeLocaleUtil.LOCALE_KEY_DELIMITER, prefix.length());
-                                if (end >= 0) {
-                                    key = key.substring(0, end);
+                    if (key.contains(prefix)) {
+                        Collection<VirtualFile> files = FileBasedIndex.getInstance().getContainingFiles(EazeLocaleKeyIndex.NAME, key, scope);
+                        if (files.size() > 0) {
+                            if (completionType == CompletionType.SMART) {
+                                if (key.startsWith(prefix)) {
+                                    int end = key.indexOf(EazeLocaleUtil.LOCALE_KEY_DELIMITER, prefix.length());
+                                    if (end >= 0) {
+                                        key = key.substring(0, end);
+                                    }
                                 }
                             }
-                        }
-                        if (key.contains(prefix)) {
                             resultSet.addElement(LookupElementBuilder.create(key));
                         }
                     }
